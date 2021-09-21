dba11bd755c4339b6cf92db84479efb3682f6468
claudiolespier <clespier@gmail.com>
2021-09-20 20:24:52 -0400

    Studio - Pending Bonus

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -7,6 +7,11 @@ public class Area {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a radius: ");
         Double r = input.nextDouble();
+        while(r<=0){
+            System.err.println("ERROR!!!! Not a valid input");
+            System.out.println("Enter a radius: ");
+            r = input.nextDouble();
+        }
         Double a = Circle.getArea(r);
         System.out.println("The area of a circle of radius 2.5 is: " + a);
    }
